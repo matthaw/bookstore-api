e971e89a34056e699e692b99d76421a5f467afab
matthaw <hardway3@protonmail.com>
2021-12-22 16:52:48 -0300

    feat: add support multi cors request

--- src/main/java/com/restapi/bookstore/config/SecurityConfig.java
+++ src/main/java/com/restapi/bookstore/config/SecurityConfig.java
@@ -34,6 +34,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .csrf().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
+                .cors()
+                .and()
                 .authorizeRequests()
                 .antMatchers("/auth/signin", "/api-docs/**", "/swagger-ui.html**").permitAll()
                 .antMatchers("/api/**").authenticated()
